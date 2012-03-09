ff500aeeca9aabb2bccdb466f7caa89181bcd654
Maximilian Krauss <max@kraussz.com>
2012-03-09 11:22:08 +0100

    Look ma, I'm under the GPL now!

--- Source/Administration/Core/DragDrop/ExtensionAttribute.cs
+++ Source/Administration/Core/DragDrop/ExtensionAttribute.cs
@@ -1,4 +1,23 @@
-﻿//HACK: this is to support a .NET language 3.5 feature in .NET 2.0
+﻿#region License
+/*
+	updateSystem.NET - Easy to use Autoupdatesolution for .NET Apps
+	Copyright (C) 2012  Maximilian Krauss <max@kraussz.com>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+//HACK: this is to support a .NET language 3.5 feature in .NET 2.0
 //see, http://www.danielmoth.com/Blog/2007/05/using-extension-methods-in-fx-20.html
 namespace System.Runtime.CompilerServices {
 	public class ExtensionAttribute : Attribute {
