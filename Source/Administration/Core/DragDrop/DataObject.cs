e74835dfa10386d43f8aee26e17c61506989c9ce
Maximilian Krauss <max@kraussz.com>
2012-03-10 10:17:02 +0100

    Added Credits to the Folks at MSDN for that nice code

--- Source/Administration/Core/DragDrop/DataObject.cs
+++ Source/Administration/Core/DragDrop/DataObject.cs
@@ -1,22 +1,5 @@
-﻿/**
- * updateSystem.NET
- * Copyright (c) 2008-2012 Maximilian Krauss <http://kraussz.com> eMail: max@kraussz.com
- *
- * This library is licened under The Code Project Open License (CPOL) 1.02
- * which can be found online at <http://www.codeproject.com/info/cpol10.aspx>
- * 
- * THIS WORK IS PROVIDED "AS IS", "WHERE IS" AND "AS AVAILABLE", WITHOUT
- * ANY EXPRESS OR IMPLIED WARRANTIES OR CONDITIONS OR GUARANTEES. YOU,
- * THE USER, ASSUME ALL RISK IN ITS USE, INCLUDING COPYRIGHT INFRINGEMENT,
- * PATENT INFRINGEMENT, SUITABILITY, ETC. AUTHOR EXPRESSLY DISCLAIMS ALL
- * EXPRESS, IMPLIED OR STATUTORY WARRANTIES OR CONDITIONS, INCLUDING
- * WITHOUT LIMITATION, WARRANTIES OR CONDITIONS OF MERCHANTABILITY,
- * MERCHANTABLE QUALITY OR FITNESS FOR A PARTICULAR PURPOSE, OR ANY
- * WARRANTY OF TITLE OR NON-INFRINGEMENT, OR THAT THE WORK (OR ANY
- * PORTION THEREOF) IS CORRECT, USEFUL, BUG-FREE OR FREE OF VIRUSES.
- * YOU MUST PASS THIS DISCLAIMER ON WHENEVER YOU DISTRIBUTE THE WORK OR
- * DERIVATIVE WORKS.
- */
+﻿//Not my work, credits go to http://blogs.msdn.com/b/adamroot/archive/2008/02/19/shell-style-drag-and-drop-in-net-wpf-and-winforms.aspx
+
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
