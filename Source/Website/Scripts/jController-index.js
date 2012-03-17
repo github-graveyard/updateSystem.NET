b97aab5b296ffb6c75b2dcb57042c69b2193b0ea
Maximilian Krauss <max@kraussz.com>
2012-03-17 14:30:50 +0100

    Fixed tipsy and added some Buttons.

--- Source/Website/Scripts/jController-index.js
+++ Source/Website/Scripts/jController-index.js
@@ -5,3 +5,17 @@
 		$('.index-hero').fadeIn('slow');
 	}, 1000);
 });	
+
+function redirdctToGitHub() {
+	document.location.href = "https://github.com/maximilian-krauss/updateSystem.NET";
+}
+function addToolTips() {
+	$('#btn-download-binaries').tipsy({
+		gravity: 's',
+		fade: true
+	});
+	$('#btn-download-sourcecode').tipsy({
+		gravity: 's',
+		fade: true
+	});
+}
